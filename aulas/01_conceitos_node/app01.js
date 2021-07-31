e9bf931ae583321ef36717590a2520f688e6005c
Mariana Lino <marianas2lino@gmail.com>
2021-07-30 22:10:39 -0300

    criação de arquivo aula 01 - exemplo execução arquivo com comando node

---
+++ aulas/01_conceitos_node/app01.js
@@ -0,0 +1,7 @@
+let nome = "Mariana";
+const idade = 32;
+let curso = "Node e Angular";
+
+console.log("Meu nome é "+ nome);
+console.log("Minha idade é "+ idade);
+console.log("O curso que estou fazendo é "+ curso);
