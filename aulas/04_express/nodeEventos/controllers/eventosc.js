58c4cf7e337e66b9a343a31d4c715e10c63521ea
Mariana Lino <marianas2lino@gmail.com>
2021-08-04 20:33:49 -0300

    Expressa (aula 3 e 4 parte 1) + comentários aulas 4 com fluxo do express

---
+++ aulas/04_express/nodeEventos/controllers/eventosc.js
@@ -0,0 +1,12 @@
+module.exports = function(app){
+    var EventosController = {
+        listar: function(request, response){
+            // render procura views na pasta views
+            // eventos é a pasta
+            // listar é o arquivo HTML com extensão .ejs
+            response.render('eventos/listar')
+        }
+    };
+
+    return EventosController;
+}
---
+++ aulas/04_express/nodeEventos/routes/home.js
@@ -0,0 +1,12 @@
+module.exports = function(app){
+    // vamos criar uma variável para referenciar nosso controller
+    var homec = app.controllers.homec;
+    var eventosc = app.controllers.eventosc;
+
+    app.get('/', homec.index);
+    // usamos o get para entender a requisição feita no browser para a aplicação e saber o que devemos fazer ou carregar ao receber a requisição
+    app.post('/login', homec.login);
+    app.get('/logout', homec.logout);
+
+    app.get('/eventos', eventosc.listar);
+}
