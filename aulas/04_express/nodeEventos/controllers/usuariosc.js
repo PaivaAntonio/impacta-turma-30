495d0b1201615560d7b70a5a29ef08d0f77670be
Mariana Lino <marianas2lino@gmail.com>
2021-08-04 23:03:27 -0300

    Fim aula 4

---
+++ aulas/04_express/nodeEventos/controllers/usuariosc.js
@@ -0,0 +1,13 @@
+module.exports = function(app){
+    var UsuariosController = {
+        novo: function(request, response){
+            var params = { user: request.session.usuarioSession }
+            // render procura views na pasta views
+            // eventos é a pasta
+            // listar é o arquivo HTML com extensão .ejs
+            response.render('usuarios/novo', params)
+        }
+    };
+
+    return UsuariosController;
+}
