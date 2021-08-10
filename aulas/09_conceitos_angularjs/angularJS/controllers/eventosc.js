234c5e1de14a84a97f5f120e66a055b68130da12
Mariana Lino <marianas2lino@gmail.com>
2021-08-09 22:37:21 -0300

    Começo código angularJS estrutura Express

---
+++ aulas/09_conceitos_angularjs/angularJS/controllers/eventosc.js
@@ -0,0 +1,8 @@
+module.exports = function(app) {
+    var EventosController = {
+        index: function(request, response) {
+            response.render('eventos/index');
+        }
+    }
+    return EventosController;
+}
