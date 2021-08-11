5eee679690da4dacc1e18668d16e3a1319f0be3b
Mariana Lino <marianas2lino@gmail.com>
2021-08-10 23:06:20 -0300

    Notas aula angular + nossa primeira aplicação + uso de diretiva estrutural

---
+++ aulas/10_angular_conceitos/app-conceitos/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppExemplo1Component } from './components/app-exemplo1/app-exemplo1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+	AppExemplo1Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ aulas/10_angular_conceitos/app-conceitos/src/app/components/app-exemplo1/app-exemplo1.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: 'app-exemplo1',
+	templateUrl: './app-exemplo1.component.html',
+	styleUrls: ['./app-exemplo1.component.scss']
+})
+export class AppExemplo1Component{
+	titulo = "Tópicos das aulas";
+
+	exibe: boolean = true;
+
+	topicos: string[] = [
+		'Conceitos do NodeJS',
+		'MVC com Express.js',
+		'Webservice - criação e consumo',
+		'MongoDB',
+		'AngularJS',
+		'Angular'
+	]
+}
